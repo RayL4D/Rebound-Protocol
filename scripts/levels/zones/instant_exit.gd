26d0cfa9168d46fa7fa14a91f4049b9e53a1c226
Kevin <sider.kevin22@gmail.com>
2026-05-21 16:55:29 +0200

    Added hide() and show() functions to manage the visibility of the portal

--- scripts/levels/zones/instant_exit.gd
+++ scripts/levels/zones/instant_exit.gd
@@ -3,10 +3,16 @@ extends Area3D
 
 @export_category("Level Transition")
 @export_file("*.tscn") var next_scene_path: String
+@onready var mesh_detect : Node = $Mesh_detector
+@onready var meshs_portal_effect : Node = $Portal/Mesh_container
 
 func _ready() -> void:
-	CollisionManager.add_missing_collisions(self)
-	body_entered.connect(_on_body_entered)
+	monitorable = false
+	monitoring = false
+	mesh_detect.hide()
+	meshs_portal_effect.hide()
+	if not body_entered.is_connected(_on_body_entered):
+		body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
@@ -14,3 +20,9 @@ func _on_body_entered(body: Node3D) -> void:
 			SceneManager.load_level(next_scene_path)
 		else:
 			push_warning("LevelExit : Aucun 'next_scene_path' n'a été défini dans l'inspecteur !")
+
+
+func activate():
+	monitoring = true
+	mesh_detect.show()
+	meshs_portal_effect.show()
--- scripts/levels/zones/level_exit.gd
+++ scripts/levels/zones/level_exit.gd
@@ -1,17 +1,21 @@
 # Portail pour sortir ET TERMINER un niveau (au niveau du score)
 extends Area3D
 
+@onready var mesh_detect : Node = $Mesh_detector
+@onready var meshs_portal_effect : Node = $Portal/Mesh_container
+
 func _ready():
-	CollisionManager.add_missing_collisions(self)
 	monitorable = false
 	monitoring = false
-	hide()
+	mesh_detect.hide()
+	meshs_portal_effect.hide()
 	if not body_entered.is_connected(_on_body_entered):
 		body_entered.connect(_on_body_entered)
 
 func activate():
 	monitoring = true
-	show()
+	mesh_detect.show()
+	meshs_portal_effect.show()
 
 func _on_body_entered(body: Node3D):
 	if body is Player:
